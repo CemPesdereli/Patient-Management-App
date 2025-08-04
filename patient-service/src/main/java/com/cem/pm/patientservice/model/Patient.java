35736ff08a4ef228fada0f8993509d1cb9199208
CemPesdereli <cempesdereli@gmail.com>
2025-08-04 18:29:13 +0300

    h2 db added patient entity added

--- patient-service/src/main/java/com/cem/pm/patientservice/model/Patient.java
+++ patient-service/src/main/java/com/cem/pm/patientservice/model/Patient.java
@@ -1,13 +1,18 @@
 package com.cem.pm.patientservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 @Entity
 public class Patient {
 
@@ -17,6 +22,20 @@ public class Patient {
 
     @NotNull
     private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate registeredDate;
 
 
 }
