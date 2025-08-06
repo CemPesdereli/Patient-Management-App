4d8baf41f384a0114007d61b5f3088e37bffbfb3
CemPesdereli <cempesdereli@gmail.com>
2025-08-07 01:10:26 +0300

    Patient controller, service, repository added

---
+++ patient-service/src/main/java/com/cem/pm/patientservice/mapper/PatientMapper.java
@@ -0,0 +1,17 @@
+package com.cem.pm.patientservice.mapper;
+
+import com.cem.pm.patientservice.dto.PatientResponseDTO;
+import com.cem.pm.patientservice.model.Patient;
+
+public class PatientMapper {
+    public static PatientResponseDTO toDTO(Patient patient) {
+        return PatientResponseDTO.builder()
+                .id(patient.getId().toString())
+                .name(patient.getName())
+                .email(patient.getEmail())
+                .address(patient.getAddress())
+                .dateOfBirth(patient.getDateOfBirth().toString())
+                .build();
+
+    }
+}
---
+++ patient-service/src/main/java/com/cem/pm/patientservice/service/PatientService.java
@@ -0,0 +1,27 @@
+package com.cem.pm.patientservice.service;
+
+import com.cem.pm.patientservice.dto.PatientResponseDTO;
+import com.cem.pm.patientservice.mapper.PatientMapper;
+import com.cem.pm.patientservice.model.Patient;
+import com.cem.pm.patientservice.repository.PatientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PatientService {
+
+    private final PatientRepository patientRepository;
+
+    public List<PatientResponseDTO> getPatients() {
+        List<Patient> patients = patientRepository.findAll();
+
+        return patients.stream()
+                .map(PatientMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+}
